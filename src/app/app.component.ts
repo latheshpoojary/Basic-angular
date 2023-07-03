932322aa2a839258645fcefbe216b1b399d9f52d
lathesh <latheshpoojary2639@gmail.com>
2023-07-03 11:11:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import {NgModel} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles:[`
+    .danger{
+      color:red;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular-learn';
+  isdiasabled:boolean=true;
+  text_color="danger";
+  isDanger =false;
+  display=false;
+  color="greenn";
+  public name="";
+  public styleObj={
+   color:"blue",
+   fontSize:"40px"
+  }
+ 
 }
